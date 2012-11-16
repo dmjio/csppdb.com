da489a8c70f02570fd681f630963f619a6ee928b
David Johnson <djohnson.m@gmail.com>
2012-11-15 21:46:13 -0600

    added login and register

--- app.py
+++ app.py
@@ -1,39 +1,111 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, session, url_for, redirect, \
+     render_template, abort, g, flash, _app_ctx_stack
 from flaskext.mysql import MySQL
+from contextlib import closing
+from hashlib import md5
+from datetime import datetime
+import time
+import os
+from werkzeug import check_password_hash, generate_password_hash
+import config
 
 app = Flask(__name__)
 mysql = MySQL()
-mysql.init_app(app)
 
-MYSQL_DATABASE_HOST = 'localhost'
-MYSQL_DATABASE_PORT = 3306
-MYSQL_DATABASE_USER = 'root'
-MYSQL_DATABASE_PASSWORD = None
-MYSQL_DATABASE_DB   = 'Twitter'
-MYSQL_DATABASE_CHARSET  = 'utf-8'
+app.config['MYSQL_DATABASE_HOST'] = config.MYSQL_DATABASE_HOST
+app.config['MYSQL_DATABASE_PORT'] = config.MYSQL_DATABASE_PORT
+app.config['MYSQL_DATABASE_USER'] = config.MYSQL_DATABASE_USER
+app.config['MYSQL_DATABASE_PASSWORD'] = config.MYSQL_DATABASE_PASSWORD
+app.config['MYSQL_DATABASE_DB'] = config.MYSQL_DATABASE_DB
+
+mysql.init_app(app)
 
 if 'SECRET_KEY' in os.environ:
     app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 else:
-    app.config['SECRET_KEY'] = 'this_should_be_configured'
-
+    app.config['SECRET_KEY'] = os.urandom(24)
 
 ###
 # Routing for your application.
 ###
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('init.sql') as f:
+            db.cursor().execute(f.read())
+        db.commit()
+
+
+def connect_db():
+    return mysql.connect()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+    g.user = None
+    if 'userid' in session:
+        g.user = query_db('select * from user where userid = ?', [session['userid']], one=True)
+    
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
+
+def query_db(query, args=(), one=False):
+    cur = g.db.cursor().execute(query, args)
+    rv = [dict((cur.description[idx][0], value)
+               for idx, value in enumerate(row)) for row in cur.fetchall()]
+    return (rv[0] if rv else None) if one else rv
+
+def format_datetime(timestamp):
+    """Format a timestamp for display."""
+    return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d @ %H:%M')
+
+def gravatar_url(email, size=80):
+    """Return the gravatar image for the given email address."""
+    return 'http://www.gravatar.com/avatar/%s?d=identicon&s=%d' % \
+        (md5(email.strip().lower().encode('utf-8')).hexdigest(), size)
 
 @app.route('/')
 def home():
     """Render website's home page."""
     return render_template('home.html')
 
-
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
+@app.route('/about/') 
+def about(): 
     return render_template('about.html')
 
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    """Registers the user."""
+    #if g.user:
+    #    return redirect(url_for('timeline'))
+    error = None
+    if request.method == 'POST':
+        if not request.form['username']:
+            error = 'You have to enter a username'
+        elif not request.form['email'] or \
+                 '@' not in request.form['email']:
+            error = 'You have to enter a valid email address'
+        elif not request.form['password']:
+            error = 'You have to enter a password'
+        elif get_user_id(request.form['username']) is not None:
+            error = 'The username is already taken'
+        else:
+            g.db.cursor().execute('''insert into user (
+              username, email, password) values (?, ?, ?)''',
+              [request.form['username'], request.form['email'],
+               generate_password_hash(request.form['password'])])
+            g.db.commit()
+            flash('You were successfully registered and can login now')
+            return redirect(url_for('login'))
+    return render_template('register.html', error=error)
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -56,6 +128,12 @@ def add_header(response):
     response.headers['Cache-Control'] = 'public, max-age=600'
     return response
 
+def get_user_id(username):
+    """Convenience method to look up the id for a username."""
+    rv = query_db('select user_id from user where username = ?',
+                  [username], one=True)
+    return rv[0] if rv else None
+
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -65,3 +143,5 @@ def page_not_found(error):
 
 if __name__ == '__main__':
     app.run(debug=True)
+    init_db()
+
